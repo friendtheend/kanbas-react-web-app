e6ba786f2acf7b8c48b8e9f9e576d6948555c60b
lu <luyifeng0131@gmail.com>
2024-09-17 11:59:58 -0700

    practice

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,50 +2,20 @@ import React from "react";
 
 export default function Lab1() {
   return (
-    <div id="wd-lab1">
-      <h2>Lab 1</h2>
-      <h3>HTML Examples</h3>
-      <div id="wd-h-tag"> ... </div>
-      <div id="wd-p-tag"> ... </div>
-      <div id="wd-lists"> ... </div>
-      <div id="wd-tables">
-        <h4>Table Tag</h4>
-        <table border={1} width="100%">
-          <thead>
-            <tr>
-              <th>Quiz</th>
-              <th>Topic</th>
-              <th>Date</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Q1</td>
-              <td>HTML</td>
-              <td>2/3/21</td>
-              <td>85</td>
-            </tr>
-            <tr>
-              <td>Q2</td>
-              <td>CSS</td>
-              <td>2/10/21</td>
-              <td>90</td>
-            </tr>
-            <tr>
-              <td>Q3</td>
-              <td>JavaScript</td>
-              <td>2/17/21</td>
-              <td>95</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan={3}>Average</td>
-              <td>90</td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-    </div>
-);}
+
+    <><h4 id="wd-dropdowns">Dropdowns</h4><h5>Select one</h5><label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br /><select id="wd-select-one-genre">
+      <option value="COMEDY">Comedy</option>
+      <option value="DRAMA">Drama</option>
+      <option selected value="SCIFI">
+        Science Fiction</option>
+      <option value="FANTASY">Fantasy</option>
+    </select><h5>Select many</h5><label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br /><select id="wd-select-many-genre" multiple>
+        <option selected value="COMEDY">Comedy</option>
+        <option value="DRAMA">Drama</option>
+        <option selected value="SCIFI">
+          Science Fiction</option>
+        <option value="FANTASY">Fantasy</option>
+      </select></>
+
+  );
+}
