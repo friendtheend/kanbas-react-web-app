676b862e13e44156ff2c66fd8580cc3b1cbe998a
lu <luyifeng0131@gmail.com>
2024-10-30 13:03:27 -0700

    3.5

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,13 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import modulesReducer from "./Courses/Modules/reducer";
+// import accountReducer from "./Account/reducer";
+// import assignmentsReducer from "./Courses/Assignments/reducer";
+
+// const store = configureStore({
+//     reducer: {
+//         modulesReducer,
+//         accountReducer,
+//         assignments: assignmentsReducer
+//     },
+// });
+// export default store;
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import accountReducer from "./Account/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
+
+const enrollmentSlice = createSlice({
+    name: "enrollments",
+    initialState: [],
+    reducers: {
+        updateEnrollments: (state, action) => {
+            return action.payload;
+        },
+    },
+});
+
+export const { updateEnrollments } = enrollmentSlice.actions;
 
 const store = configureStore({
     reducer: {
         modulesReducer,
         accountReducer,
-        assignments: assignmentsReducer
+        assignments: assignmentsReducer,
+        enrollmentReducer: enrollmentSlice.reducer,
     },
 });
+
 export default store;
